8979fab48bcdf4f36ff8cd781bc446956be59437
Henry.rhee <>
2020-10-13 22:07:20 +0900

    코드정제

--- src/ch008_moving_features/ex005_replace_inline_code_with_function_call/states001.js
+++ src/ch008_moving_features/ex005_replace_inline_code_with_function_call/states001.js
@@ -2,7 +2,7 @@ function includeMA(states) {
   let appliesToMass = false;
   for (const s in states) {
     if(s === "MA") appliesToMass = true;
-    return true;
+    return appliesToMass;
   }
   return appliesToMass;
 }
