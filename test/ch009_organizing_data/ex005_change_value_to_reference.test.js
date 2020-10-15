b5e0d578d09c41f0474b80a6261c0b7c9a13499f
youngbeomrhee <youngbeomrhee@greencar.co.kr>
2020-10-15 09:04:03 +0900

    chapter 009.004 테스트 코드 수정

--- test/ch009_organizing_data/ex005_change_value_to_reference.test.js
+++ test/ch009_organizing_data/ex005_change_value_to_reference.test.js
@@ -1,11 +1,11 @@
 const { expect, assert } = require('chai');
 const { Order, Customer } = require('../../src/ch009_organizing_data/ex005_change_value_to_reference/order001');
-const { Order: Order2, initialize } = require('../../src/ch009_organizing_data/ex005_change_value_to_reference/order002');
+const { Order: Order2, initialize, findCustomer } = require('../../src/ch009_organizing_data/ex005_change_value_to_reference/order002');
 
 describe('order', () => {
   it('order001', () => {
     const o1 = new Order({number: 1, customer: new Customer(1)});
-    const o2 = new Order({number: 2, customer: new Customer(2)});
+    const o2 = new Order({number: 2, customer: new Customer(1)});
 
     expect(o1.customer === o2.customer).equals(false);
   });
@@ -14,9 +14,9 @@ describe('order', () => {
     const Order = Order2;
     initialize();
     const o1 = new Order({number: 1, customer: new Customer(1)});
-    const o2 = new Order({number: 2, customer: new Customer(2)});
+    const o2 = new Order({number: 2, customer: new Customer(1)});
 
-    expect(o1.customer === o2.customer).equals(false);
+    expect(findCustomer(1) === findCustomer(1)).equals(true);
   });
 
 });
